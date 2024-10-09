57ac6b674e73df0b18dab43abdbb85596368ee7e
Joshua Blackmore <joshuablackmore@gmail.com>
2024-10-09 14:11:10 +0100

    challenge complete

--- lib/playlist.py
+++ lib/playlist.py
@@ -6,7 +6,18 @@ class Playlist:
 
         self.playlist = {}
 
-    def add(self, artist, trackname):
+    def add(self, trackname, artist="Unknown Artist"):
+        if not isinstance(trackname, str) or not isinstance(artist, str):
+            raise Exception("Trackname and/or Artist must a string.")
+        
+        trackname = trackname.strip()
+        artist = artist.strip()
+        
+        if trackname == "":
+            raise Exception("Please pass a none empty string.")
+        elif artist == "":
+            artist = "Unknown Artist"
+        
         self.playlist.setdefault(artist,[]).append(trackname)
 
     def list_all(self):
--- tests/test_playlist.py
+++ tests/test_playlist.py
@@ -1,5 +1,6 @@
 from lib.playlist import *
 
+import pytest
 
 def test_add_artist_and_song_stored_in_dictionary():
     playlist = Playlist()
@@ -8,14 +9,37 @@ def test_add_artist_and_song_stored_in_dictionary():
     
 def test_add_artist_and_song_stored_in_dictionary():
     playlist = Playlist()
-    playlist.add("Pink Floyd","time")
+    playlist.add("time", "Pink Floyd")
     assert playlist.playlist == {"Pink Floyd":["time"]}
 
 
 
 def test_list_all_returns_formatted_string():
     playlist = Playlist()
-    playlist.add("Pink Floyd","time")
-    playlist.add("Pink Floyd", "comfortably numb")
-    playlist.add("Rage", "Killing in the name")
-    assert playlist.list_all() == "time by Pink Floyd, comfortably numb by Pink Floyd, Killing in the name by Rage"
+    playlist.add("time", "Pink Floyd")
+    playlist.add("comfortably numb", "Pink Floyd")
+    playlist.add("Killing in the name", "Rage")
+    playlist.add("Suck my kiss")
+    assert playlist.list_all() == "time by Pink Floyd, comfortably numb by Pink Floyd, Killing in the name by Rage, Suck my kiss by Unknown Artist"
+
+@pytest.mark.parametrize("bad_inputs", [int,float, None, []])
+def test_exception_raised_if_trackname_or_artist_is_not_a_string(bad_inputs):
+    playlist = Playlist()
+    with pytest.raises(Exception) as e:
+        playlist.add(bad_inputs,bad_inputs)
+    assert str(e.value) == "Trackname and/or Artist must a string."
+
+def test_add_does_not_except_empty_string():
+    playlist = Playlist()
+    with pytest.raises(Exception) as e:
+        playlist.add("","")
+    assert str(e.value) == "Please pass a none empty string."
+    
+def test_add_cleans_up_dodgy_strings():
+    playlist = Playlist()
+    playlist.add("  time     ", "  Pink Floyd      ")
+    playlist.add(" killing in the name   ","     ")
+    
+    assert playlist.list_all() == "time by Pink Floyd, killing in the name by Unknown Artist"
+    
+    
